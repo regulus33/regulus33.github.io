23f9d18646811c2d6580b026f78492c676c2f4aa
zack <zacharyrowden89@gmail.com>
2020-10-01 11:31:46 +0200

    fix link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby'
+import { Link } from "gatsby"
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
@@ -14,7 +14,7 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><Link to="/work">Work</a></li>
+                <li><Link to="/work">Work</Link></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
             </ul>
         </nav>
